b9d39f7adedeb324e8a61730ce9aaa5b9bc2d78b
din0025-mattyv <102110216+din0025-mattyv@users.noreply.github.com>
2022-04-06 15:04:47 +1000

    Add files via upload

---
+++ SofDev SAC task THREE/SofDev SAC task THREE/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SofDev_SAC_task_THREE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.cbbxDisplay = new System.Windows.Forms.ComboBox();
+            this.grdvwData = new System.Windows.Forms.DataGridView();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.lblSearch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grdvwData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(595, 39);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(174, 26);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // cbbxDisplay
+            // 
+            this.cbbxDisplay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbxDisplay.FormattingEnabled = true;
+            this.cbbxDisplay.Items.AddRange(new object[] {
+            "Rating",
+            "Subject",
+            "Textbook"});
+            this.cbbxDisplay.Location = new System.Drawing.Point(189, 37);
+            this.cbbxDisplay.Name = "cbbxDisplay";
+            this.cbbxDisplay.Size = new System.Drawing.Size(121, 28);
+            this.cbbxDisplay.Sorted = true;
+            this.cbbxDisplay.TabIndex = 1;
+            this.cbbxDisplay.SelectedIndexChanged += new System.EventHandler(this.cbbxDisplay_SelectedIndexChanged);
+            // 
+            // grdvwData
+            // 
+            this.grdvwData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdvwData.Location = new System.Drawing.Point(12, 81);
+            this.grdvwData.Name = "grdvwData";
+            this.grdvwData.ReadOnly = true;
+            this.grdvwData.RowHeadersWidth = 62;
+            this.grdvwData.RowTemplate.Height = 28;
+            this.grdvwData.Size = new System.Drawing.Size(776, 357);
+            this.grdvwData.TabIndex = 2;
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Location = new System.Drawing.Point(86, 40);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(97, 20);
+            this.lblFilter.TabIndex = 3;
+            this.lblFilter.Text = "Select Filter:";
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(474, 42);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(115, 20);
+            this.lblSearch.TabIndex = 4;
+            this.lblSearch.Text = "Refine Search:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lblFilter);
+            this.Controls.Add(this.grdvwData);
+            this.Controls.Add(this.cbbxDisplay);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdvwData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ComboBox cbbxDisplay;
+        private System.Windows.Forms.DataGridView grdvwData;
+        private System.Windows.Forms.Label lblFilter;
+        private System.Windows.Forms.Label lblSearch;
+    }
+}
+
---
+++ SofDev SAC task THREE/SofDev SAC task THREE/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SofDev_SAC_task_THREE
+{
+    public partial class Form1 : Form
+    {
+        //create list where every item is an object in Search
+        List<Search> table = new List<Search>();
+
+        BindingSource bindSource = new BindingSource();
+        
+        //initialise filter variable
+        string filter;
+        public Form1()
+        {
+            InitializeComponent();
+            loadCSV();
+            
+            //set data source for the dataGridView
+            bindSource.DataSource = table;
+            grdvwData.DataSource = bindSource;
+            bindSource.ResetBindings(false);
+
+            
+
+        }
+
+        private void cbbxDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //set filter variable
+            filter = cbbxDisplay.Text;
+        }
+
+        public void loadCSV()
+        {
+            //load file and create list
+            string filePath = "C:/#DataSources/Task3_Shop_Data.csv";
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach(string line in lines)
+            {
+                List<string> items = line.Split(',').ToList();
+
+                //empty object in Search class
+                Search n = new Search();
+                //create new objects with the splits from items list
+                n.Title = items[0];
+                n.Subject = items[1];
+                n.Seller = items[2];
+                n.Buyer = items[3];
+                n.buyPrice = float.Parse(items[4]);
+                n.salePrice = items[5];
+                n.Rating = items[6];
+
+                table.Add(n);
+            }
+        }
+
+        public List<Search> FindSearch(string target, string filter)
+        {
+            List<Search> results = new List<Search>();
+            foreach (Search n in table)
+            {
+                //check filter result first
+                if (filter == "Subject")
+                {
+                    //makes sure search is for incomplete/inexact also
+                    if (n.Subject.ToLower().Contains(target.ToLower()))
+                    {
+                        results.Add(n);
+                    }
+                }
+                if(filter == "Textbook")
+                {
+                    //makes sure search is for incomplete/inexact also
+                    if (n.Title.ToLower().Contains(target.ToLower()))
+                    {
+                        results.Add(n);
+                    }
+                }
+                //sort rating, not search by rating
+                //couldn't get to work :(
+                if (filter == "Rating")
+                {
+                    List<Search> copyRate = table;
+                    foreach (Search e in copyRate)
+                    {
+                        if (e.Rating == "none")
+                        {
+                            e.Rating = 0.ToString();
+                        }
+                    }
+
+                    int min = 0;
+
+                    for (int i = 0; i < copyRate.Count() - 1; i++)
+                    {
+                        min = i; //minimum value changes every runthrough,
+                                 //doesn't alter the already sorted values
+                        for (int j = i + 1; j < copyRate.Count(); j++)
+                        {
+                            if (copyRate[j] < copyRate[min])
+                            {
+                                min = j;
+                            }
+
+                        }
+                        int pret = copyRate[min]; //double of min's value
+                        copyRate[min] = copyRate[i];
+                        copyRate[i] = pret;
+
+
+                    }
+
+                    
+
+                }
+            }
+            return results;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            //sets target parameter as text box text
+            List<Search> r = FindSearch(txtSearch.Text, filter);
+
+            //change binding source's data source so output alters
+            bindSource.DataSource = r;
+            grdvwData.DataSource = bindSource;
+            bindSource.ResetBindings(false);
+        }
+    }
+}
